4a7c48050e59d97886dd892022ce7e09c115b8d7
Hécate Moonlight <hecate+github@glitchbra.in>
2022-02-17 20:17:17 +0100

    Split the crypto-rng bindings into their own repo

---
+++ test/Main.hs
@@ -0,0 +1,44 @@
+module Main where
+
+import Control.Monad
+import Data.ByteString
+import Effectful
+import Effectful.Reader.Static
+import Test.Tasty
+import Test.Tasty.HUnit
+
+import Effectful.Crypto.RNG
+
+main :: IO ()
+main = do
+  defaultMain $ testGroup "Crypto-RNG Bindings"
+    [ testCase "Genering random bytes wrapped in a newtype" testRandomBytes
+    , testCase "Generating a random number within a range from Reader" testRandomNumber
+    ]
+
+testRandomNumber :: Assertion
+testRandomNumber = runEff $ do
+  cryptoState <- newCryptoRNGState
+  void $ runCryptoRNG cryptoState
+            . runReader ((10, 20) :: (Int, Int))
+            $ generatingRandomNumber
+
+generatingRandomNumber :: (CryptoRNG :> es, Reader (Int, Int) :> es) => Eff es Int
+generatingRandomNumber = do
+  bounds <- ask
+  randomR bounds
+
+---
+
+testRandomBytes :: Assertion
+testRandomBytes = runEff $ do
+  cryptoState <- newCryptoRNGState
+  void $ runCryptoRNG cryptoState generateUID
+
+newtype UID = UID ByteString
+  deriving newtype (Show, Eq, Ord)
+
+generateUID :: (CryptoRNG :> es ) => Eff es UID
+generateUID = do
+  bytes <- randomBytes 8
+  pure $ UID bytes
